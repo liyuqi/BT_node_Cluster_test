4d669df7433cbe7691b6b45e6e355787933ad237
liyu <am997vivian@gmail.com>
2014-09-15 11:23:37 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,9 +14,26 @@ process.on('message', function(id, handle, mongodb) {
 });*/
 
 var http = require('http');
+var util = require('util');
+var fs = require('fs');
+
+var monk = require('monk');
+var db = monk('127.0.0.1:27017/test');
+var collection = db.get('clusterLog');
 
 process.on('message', function(id, handle) {
     http.createServer(function(req, res) {
+		/*
+		var clusterLog = "\n worker "+id;
+		fs.appendFile('./clusterLog.txt', clusterLog, function (err) {
+			if(err) console.log("cluster fail");
+		});
+		*/
+		
+		collection.insert({"worker":id}, function(err, clusterLog){if(err);});
+		
+		//db.clusterLog.aggregate({$group:{_id:{ worker:'$worker'}, count:{$sum:1}}})
+		
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Worker ' + id);
     }).listen(handle);
